1e77534802f078b2443364c429a036d9457d7181
Sammy Alali <wwehdv9@gmail.com>
2022-11-17 02:08:45 +0100

    Commit first

---
+++ Domain/Models/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Item
+    {
+        [Key] //These attributes are for database generation
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        [Required]
+        public string Name { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set;}
+
+        public double Price { get; set; }
+
+        public string Description { get; set; }
+
+        public string PhotoPath { get; set; }
+
+
+    }
+}
